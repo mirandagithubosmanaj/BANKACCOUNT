8ac80929d16bdacae2a1a57ec2f4df7928831cf7
Ajete Krasniqi <123266026+AjeteKr@users.noreply.github.com>
2023-09-17 22:19:47 +0200

    fixed_database

--- DATABASEE/Context/ApplicationDbContext.cs
+++ DATABASEE/Context/ApplicationDbContext.cs
@@ -25,8 +25,8 @@ namespace DATABASEE.Context
         
 
             modelBuilder.Entity<Deposits>();
-            modelBuilder.Entity<Transfer>();
-            modelBuilder.Entity<User>();
+            modelBuilder.Entity<Transfers>();
+            modelBuilder.Entity<Users>();
             modelBuilder.Entity<UserRoles>();
             modelBuilder.Entity<Withdraws>();
 
